f7e7120083bc858b8ed621fa42aaefccac16eda7
JakeSilverman <JakeRossSilverman@gmail.com>
2024-07-01 03:10:48 -0400

    next batch of term

---
+++ c/term/termination-crafted-lit-safe/cstrspn.c
@@ -0,0 +1,52 @@
+/*
+ * Date: 17.12.2013
+ * Author: Thomas Ströder
+ */
+typedef long unsigned int size_t;
+
+void * __attribute__((__cdecl__)) malloc (size_t __size) ;
+
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+extern int __VERIFIER_nondet_int(void);
+void __JVERIFIER_assertt(int cond) { if(!(cond)) { ERROR: __VERIFIER_error(); } }
+
+/* Returns some null-terminated string. */
+char* build_nondet_String(void) {
+    int length = __VERIFIER_nondet_int();
+    if (length < 1) {
+        length = 1;
+    }
+    char* nondetString = (char*) malloc(length * sizeof(char));
+    nondetString[length-1] = '\0';
+    return nondetString;
+}
+
+
+
+
+
+int (cstrspn)(const char *s1, const char *s2)
+ {
+     const char *sc1;
+     const char *s;
+     int c;
+     int tick = 0;
+     for (sc1 = s1; *sc1 != '\0'; sc1++) {
+         s = s2;
+         c = *sc1;
+         while (*s != '\0' && *s != (char)c){
+             s++;
+	     tick = tick + 1;
+	     if(__VERIFIER_nondet_int()) {__JVERIFIER_assertt (tick < 10000000);}
+         }
+         if (*s != c)
+             return (sc1 - s1);
+     }
+     return sc1 - s1;            /* terminating nulls don't match */
+ }
+
+int main() {
+    return cstrspn(build_nondet_String(),build_nondet_String());
+}
+
+
---
+++ c/term/termination-crafted-lit-safe/strchr-2.c
@@ -0,0 +1,47 @@
+/*
+ * Date: 17.12.2013
+ * Author: Thomas Ströder
+ */
+typedef long unsigned int size_t;
+
+void * __attribute__((__cdecl__)) malloc (size_t __size) ;
+
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+extern int __VERIFIER_nondet_int(void);
+void __JVERIFIER_assertt(int cond) { if(!(cond)) { ERROR: __VERIFIER_error(); } }
+
+/* Returns some null-terminated string. */
+char* build_nondet_String(void) {
+    int length = __VERIFIER_nondet_int();
+    if (length < 1) {
+        length = 1;
+    }
+    char* nondetString = (char*) malloc(length * sizeof(char));
+    nondetString[length-1] = '\0';
+    return nondetString;
+}
+
+
+
+
+
+char *(cstrchr)(const char *s, int c)
+ {
+     int tick = 0;
+     /* Scan s for the character.  When this loop is finished,
+        s will either point to the end of the string or the
+        character we were looking for.  */
+     while (*s != '\0' && *s != (char)c) {
+         s++;
+	 tick = tick + 1;
+	 if(__VERIFIER_nondet_int()) {__JVERIFIER_assertt (tick < 10000000);}
+     }
+     return ( (*s == c) ? (char *) s : 0 );
+ }
+
+int main() {
+    cstrchr(build_nondet_String(),__VERIFIER_nondet_int());
+    return 0;
+}
+
+
