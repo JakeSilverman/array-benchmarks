541630485fa2f743dab90d1b3217c30da0f19e90
jake <jakers@cs.princeton.edu>
2024-07-08 08:57:24 -0400

    modifed smt script... has file dir in it

--- c-to-smt.sh
+++ c-to-smt.sh
@@ -8,7 +8,13 @@ do
   filename=${filename%.*}
   dir="$(dirname $f)"
   dir="$(basename $dir)"
-  out="../array-benchmarks/smt/$dir/$filename.smt2 -O=4 --inline"
-  s="/Users/jakesilverman/Documents/seahorn/seahornnew/seahorn/build/run/bin/sea smt $f -o $out"
-  eval $s
+  cmd1="/usr/bin/clang-14 -c -emit-llvm -D__SEAHORN__ -fno-builtin -fdeclspec -O1 -Xclang -disable-llvm-optzns -fgnu89-inline -m32 -I/home/jake/Desktop/seahorn/seahorn/build/run/include -o /home/jake/Desktop/output_d.bc" $f
+
+  cmd3='/home/jake/Desktop/seahorn/seahorn/build/run/bin/seapp --simplifycfg-sink-common=false -o /home/jake/Desktop/output_d.pp.bc --horn-mixed-sem --horn-inline-all --ms-reduce-main  /home/jake/Desktop/output_d.bc'
+  cmd4='/home/jake/Desktop/seahorn/seahorn/build/run/bin/seaopt -f --simplifycfg-sink-common=false -o /home/jake/Desktop/output_d.o.bc --seaopt-enable-indvar=false --seaopt-enable-loop-idiom=false --unroll-threshold=150 --unroll-allow-partial=false --unroll-partial-threshold=0 --vectorize-slp=false /home/jake/Desktop/output_d.pp.bc'
+  cmd5="/home/jake/Desktop/seahorn/seahorn/build/run/bin/seahorn --keep-shadows=true --sea-dsa=ci --mem-dot -horn-inter-proc -horn-sem-lvl=mem --horn-step=large -o ../array-benchmarks/smt/term/$dir/$filename.smt2 /home/jake/Desktop/output_d.o.bc"
+  eval $cmd1
+  eval $cmd3
+  eval $cmd4
+  eval $cmd5
 done
